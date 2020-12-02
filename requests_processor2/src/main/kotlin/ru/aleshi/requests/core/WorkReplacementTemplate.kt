dad534dfdd8db8345c5257223cc0454aaa916db4
alexandershirokih <alexandrshirokih@mail.ru>
2020-12-02 21:40:58 +0300

    remove legacy request processor

--- requests_processor2/src/main/kotlin/ru/aleshi/requests/core/WorkReplacementTemplate.kt
+++
@@ -1,9 +0,0 @@
-package ru.aleshi.requests.core
-
-class WorkReplacementTemplate(
-    val pattern: String,
-    val shortName: String,
-    val fullName: String
-) {
-    constructor(default: String) : this(default, default, default)
-}
--- requests_processor2/src/main/kotlin/ru/aleshi/requests/data/ProcessResult.kt
+++
@@ -1,8 +0,0 @@
-package ru.aleshi.requests.data
-
-data class ProcessResult<T>(
-    val data: T?,
-    val error: String = "",
-    val stackTrace: String = ""
-)
-
