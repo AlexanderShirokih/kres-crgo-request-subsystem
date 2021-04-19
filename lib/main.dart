32237e31874c165c6835c4a608f8248229bcf819
alexandershirokih <alexandrshirokih@mail.ru>
2021-04-19 21:32:22 +0300

    add localizations support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kres_requests2/app_module.dart';
 
@@ -9,6 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('en', ''),
+          const Locale('ru', ''),
+        ],
         title: 'Заявки КРЭС 2.0',
         theme: ThemeData(
           primaryColor: Colors.blue,
